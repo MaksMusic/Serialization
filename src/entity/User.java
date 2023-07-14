02e23430ecc8f2ce59935a0c4066dc8cc9e7760a
MaksMusic <Maksmusic05@yandex.ru>
2023-07-15 01:23:22 +0300

    Initial commit

---
+++ src/entity/User.java
@@ -0,0 +1,48 @@
+package entity;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String name;
+    private  int age;
+    private String time;
+
+    public User(String name, int age, String time) {
+        this.name = name;
+        this.age = age;
+        this.time = time;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", time='" + time + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
